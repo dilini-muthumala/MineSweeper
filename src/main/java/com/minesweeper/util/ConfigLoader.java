142ad88b4277a2b34abaf519835f01863eda50fd
dilini-muthumala <diliniasanga@gmail.com>
2025-06-22 11:40:52 +0800

    Add ConfigLoader class

---
+++ src/main/java/com/minesweeper/util/ConfigLoader.java
@@ -0,0 +1,36 @@
+package com.minesweeper.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * Loader class for providing the Game configurations
+ */
+public class ConfigLoader {
+    private static final int MAX_DEFAULT_GRID_SIZE = 26;
+    private static final double MAX_MINE_DENSITY = 0.35;
+
+    private static final Properties props = new Properties();
+
+    static {
+        try (InputStream input = ConfigLoader.class.getClassLoader().getResourceAsStream("config.properties")) {
+            if (input != null) {
+                props.load(input);
+            } else {
+                throw new RuntimeException("Missing config.properties file");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load configuration", e);
+        }
+    }
+
+    //todo: for future improvement
+    public static int getMaxGridSize() {
+        return Integer.parseInt(props.getProperty("max.grid.size", String.valueOf(MAX_DEFAULT_GRID_SIZE)));
+    }
+
+    public static double getMaxMineDensity() {
+        return Double.parseDouble(props.getProperty("max.mine.density", String.valueOf(MAX_MINE_DENSITY)));
+    }
+}
